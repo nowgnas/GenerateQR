103bd53e5bc9dcd8c714529fd08581db2f8fbd90
s-wlii <marshmello.sw@gmail.com>
2021-10-24 21:06:38 +0900

    generate qrcode

--- generate/make_QR.py
+++ generate/make_QR.py
@@ -1,14 +1,12 @@
-from PIL import Image
-import qrcode
 import pandas as pd
+import qrcode
 
 UserData = pd.read_excel('../data/tave_member.xlsx')
 
 for _iter in UserData.iterrows():
-    print(_iter[1]['phone'])
     generation = _iter[1]['gen']
     UserName = _iter[1]['name']
-    UserPhone = _iter[1]['phone']
-    UserDataString = f'gen: {generation} name: {UserName} phone: {UserPhone}'
+    UserPhone = _iter[1].iloc[2]
+    UserDataString = f'gen: {generation} name: {UserName} phone: 0{UserPhone}'
     QRGeneration = qrcode.make(UserDataString)
     QRGeneration.save(f'../data/qrImg/usr_qrCode/{UserName}.png')
