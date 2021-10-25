e8633d874a7793ffb7aace85a93695bea63b9a61
s-wlii <marshmello.sw@gmail.com>
2021-10-25 22:53:01 +0900

    amend file

--- generate/make_QR.py
+++ generate/make_QR.py
@@ -10,3 +10,4 @@ for _iter in UserData.iterrows():
     UserDataString = f'gen: {generation} name: {UserName} phone: 0{UserPhone}'
     QRGeneration = qrcode.make(UserDataString)
     QRGeneration.save(f'../data/qrImg/usr_qrCode/{UserName}.png')
+# 소켓 통신 한번 시도해보기
